b76fb27cd3e2828dedbeb604b1bc30cceb04c875
eishanjindal <eishanjindal@ninjacart.com>
2020-11-21 19:22:15 +0530

    first commit

---
+++ java-reference-document.java
@@ -0,0 +1,15 @@
+@Override
+public boolean equals(Point another){
+	//If these objects are same
+	if(another == this)
+		return true;
+	
+	//Check if another is an instance of Point
+	if(!(another instanceof Point))
+		return false;
+
+	//Logic for equality
+	
+
+
+}
