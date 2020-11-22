4c8fef443e36c63c3e615528c001d671933cf633
kyro-xen <eishanjindal88@gmail.com>
2020-11-22 11:32:16 +0530

    equals and hashcode function

--- java-reference-document.java
+++ java-reference-document.java
@@ -1,15 +1,28 @@
 @Override
 public boolean equals(Point another){
-	//If these objects are same
+	//To check If these objects are same
 	if(another == this)
 		return true;
 	
-	//Check if another is an instance of Point
+	//To check if another is an instance of Point
 	if(!(another instanceof Point))
 		return false;
 
 	//Logic for equality
+	Point p = (Point) another;
+    if(p.coord == this.coord)
+    	return true;
+            
+    if(p.coord[0] == this.coord[0] && p.coord[1] == this.coord[1])
+    	return true;
 	
+    return false;
+}
 
-
+@Override
+public final int hashCode() {
+	int result = 17;
+	if (coord != null)
+		result = 31 * result + Arrays.hashCode(this.coord);    
+	return result;
 }
