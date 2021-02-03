16e0927f447e415d4bb520204e988b2603cbd7c5
kyroxen <eishanjindal88@gmail.com>
2021-02-03 12:00:36 +0530

    Add packages, refactor

--- coreJava/Reference.java
+++ coreJava/Reference.java
@@ -1,4 +1,12 @@
-import java.util.*;
+package coreJava;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.regex.Pattern;
 
 /**
